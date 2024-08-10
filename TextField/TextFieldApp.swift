f669eb988129cdacf7f52cca9a0ecf0cd33bc3f2
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2024-08-10 20:39:29 +0530

    Initial Commit

---
+++ TextField/TextFieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldApp.swift
+//  TextField
+//
+//  Created by Sajed Shaikh on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
